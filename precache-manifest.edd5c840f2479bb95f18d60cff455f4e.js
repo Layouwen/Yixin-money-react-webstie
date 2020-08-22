49e3f145aefcf7ccbc212a76fe420dc9f15b5686
liangyouwen <layouwen@gmail.com>
2020-08-22 16:00:46 +0800

    init

---
+++ precache-manifest.edd5c840f2479bb95f18d60cff455f4e.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "ad194844ebaa15c67b4040e77a790ff2",
+    "url": "/Yixin-money-react-website/index.html"
+  },
+  {
+    "revision": "6e78b24965858ba29fa0",
+    "url": "/Yixin-money-react-website/static/css/main.a060e60c.chunk.css"
+  },
+  {
+    "revision": "b291e8c0b2020e783908",
+    "url": "/Yixin-money-react-website/static/js/2.c45d3c2b.chunk.js"
+  },
+  {
+    "revision": "99406820fc9e02f862dbd66e126ca114",
+    "url": "/Yixin-money-react-website/static/js/2.c45d3c2b.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "6e78b24965858ba29fa0",
+    "url": "/Yixin-money-react-website/static/js/main.ed9fc5e1.chunk.js"
+  },
+  {
+    "revision": "b06f72b8932770f60376",
+    "url": "/Yixin-money-react-website/static/js/runtime-main.4e8ef5d6.js"
+  }
+]);
